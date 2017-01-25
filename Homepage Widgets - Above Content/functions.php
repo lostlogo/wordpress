2a48a41ec5ba68bd0d88c50e2f1606b77eecf1b3
lostlogo <lostlogo@gmail.com>
2017-01-24 18:13:10 -0800

    Move to widgets folder

---
+++ Homepage Widgets - Above Content/functions.php
@@ -0,0 +1,65 @@
+// Homepage Russian Map
+register_sidebar( array(
+    'name'         => __( 'Russian Map', 'fltext' ),
+    'id'           => 'top-home-map',
+    'description'  => __( 'Home Russian Map', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
+
+// Homepage Widget Title Area
+register_sidebar( array(
+    'name'         => __( 'Home Title', 'fltext' ),
+    'id'           => 'top-home-title',
+    'description'  => __( 'Home Title', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
+
+// Homepage Widget Area
+register_sidebar( array(
+    'name'         => __( 'Home 1', 'fltext' ),
+    'id'           => 'top-home',
+    'description'  => __( 'Home Widget', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
+
+// Homepage Widget Area
+register_sidebar( array(
+    'name'         => __( 'Home 2', 'fltext' ),
+    'id'           => 'top-home2',
+    'description'  => __( 'Home Widget 2.', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+     'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
+
+// Homepage Widget Area
+register_sidebar( array(
+    'name'         => __( 'Home 3', 'fltext' ),
+    'id'           => 'top-home3',
+    'description'  => __( 'Home Widget 3', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
+
+// Homepage Widget Area
+register_sidebar( array(
+    'name'         => __( 'Home 4', 'fltext' ),
+    'id'           => 'top-home4',
+    'description'  => __( 'Home Widget 4', 'fltext' ),
+    'before_widget'=> '<div id="%1$s" class="widget-home">',
+    'after_widget' => '</div>',
+     'before_title' => '<h3 class="widget-title">',
+    'after_title'  => '</h3>',
+) );
